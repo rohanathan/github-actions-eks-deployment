ff485f5773835ca679db39c1e63186c0b42622ca
Rohanathan Suresh <rohanathan.s@hotmail.com>
2025-10-07 18:03:01 +0100

    Added new CI

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -5,7 +5,7 @@ module "eks" {
   cluster_name                   = "eks-demo-cluster"
   cluster_version                = "1.30"
   cluster_endpoint_public_access = true
-  enable_irsa                    = true 
+  enable_irsa                    = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
@@ -19,35 +19,28 @@ module "eks" {
       capacity_type  = "ON_DEMAND"
     }
   }
-  # Given IAM user cluster-admin access via EKS Access Entries
+
   access_entries = {
     admin = {
       principal_arn = "arn:aws:iam::897545368009:user/terraform-org-admin"
-
-      policy_associations = [
-        {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = {
-            type = "cluster"
-          }
-        }
-      ]
+      policy_associations = [{
+        policy_arn   = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+        access_scope = { type = "cluster" }
+      }]
     }
-    # GitHub Actions OIDC role used by CD
+
     gha_role = {
-      principal_arn = "arn:aws:iam::897545368009:role/gha-eks-deploy-role"
-      policy_associations = [
-        {
-          policy_arn  = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = { type = "cluster" }
-        }
-      ]
+      # reference the role you create above, so TF knows the order
+      principal_arn = aws_iam_role.gha_deploy_role.arn
+      policy_associations = [{
+        policy_arn   = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+        access_scope = { type = "cluster" }
+      }]
     }
   }
 
+  tags = { Project = "eks-demo" }
 
-  tags = {
-    Project = "eks-demo"
-  }
+  # make creation order explicit
+  depends_on = [aws_iam_role.gha_deploy_role]
 }
-
--- terraform/ingress-nginx.tf
+++ terraform/ingress-nginx.tf
@@ -7,6 +7,7 @@ data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
 }
 
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
